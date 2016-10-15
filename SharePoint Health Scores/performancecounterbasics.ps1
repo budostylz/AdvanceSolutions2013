9d5af0e630b1fe538b420e53678aace7be442206
budostylz <lewis.shaun1@outlook.com>
2016-10-15 19:46:49 -0400

    'commit'

---
+++ SharePoint Health Scores/performancecounterbasics.ps1
@@ -0,0 +1,26 @@
+﻿
+
+<#
+cls
+#Add-PSSnapin 'Microsoft.SharePoint.PowerShell'
+
+#>
+
+#View Performance Counters for Web Application
+#Get-SPWebApplicationHttpThrottlingMonitor http://vsalm/
+
+#Adding Custom Monitor TEST THIS ONLY IN DEV ENVIRONMENTS
+<#$throttle = (Get-SPWebApplication http://vsalm).HttpThrottleSettings
+$throttle.AddPerformanceMonitor("Processor", "% Processor Time", "_Total", 10, $true)
+$throttle.Update()#>
+
+#Refresh Interval
+<#$throttle = (Get-SPWebApplication http://vsalm).HttpTrottleSettings
+$throttle.RefreshInterval#>
+
+#Prevent Http Throttling
+<#$classifier = New-Object Microsoft.SharePoint.Utilities.SPHttpUserAgentAndMethodClassifier -Args "", "POST", "HttpMethodMatch", "Never"
+$throttle = (Get-SPWebApplication http://vsalm).HttpThrottleSettings
+$throttle.AddThrottleClassifier($classifier)
+$throttle.Update()#>
+
